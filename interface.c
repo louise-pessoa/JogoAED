59fc8c9db6e501227a7435046dbe64a80aa1289f
Pdob <pdob@gmail.com>
2026-05-15 00:31:14 -0300

    fix(ui): corrige includes e escape de caracteres no menu

--- interface.c
+++ interface.c
@@ -2,60 +2,58 @@
 #include "interface.h"
 #include "jogo.h"
 
-void exibir_menu() {
+void exibir_menu(void) {
 
     int opcao;
 
     do {
 
         printf("\n");
-        printf("================================\n");
-        printf("       comida de mainha\n");
-        printf("================================\n");
-        printf("1 - jogar\n");
-        printf("2 - testar receita\n");
-        printf("3 - ver pontuacao\n");
-        printf("0 - sair\n");
-        printf("================================\n");
+        printf("====================================\n");
+        printf("         comida de mainha\n");
+        printf("====================================\n");
+        printf("[1] jogar\n");
+        printf("[2] testar receita\n");
+        printf("[3] ver pontuacao\n");
+        printf("[4] creditos\n");
+        printf("[0] sair\n");
+        printf("====================================\n");
 
-        printf("escolha: ");
+        printf("escolha uma opcao: ");
         scanf("%d", &opcao);
 
         switch(opcao) {
 
             case 1:
-
                 // inicia uma nova partida
                 iniciar_jogo();
-
-                printf("\njogo iniciado!\n");
+                printf("\npartida iniciada.\n");
                 break;
 
             case 2:
-
-                // simula uma receita simples
+                // teste rapido da mecanica
                 estado.passos_total = 3;
 
                 _avancar_progresso();
                 _avancar_progresso();
                 _avancar_progresso();
-
                 break;
 
             case 3:
-
                 printf("\npontuacao atual: %d\n", estado.pontuacao);
                 break;
 
-            case 0:
+            case 4:
+                printf("\nprojeto de aed - comida de mainha\n");
+                break;
 
-                printf("\nencerrando...\n");
+            case 0:
+                printf("\nencerrando jogo...\n");
                 break;
 
             default:
-
                 printf("\nopcao invalida.\n");
-
+                break;
         }
 
     } while(opcao != 0);
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
+#include "jogo.h"
 #include "interface.h"
 
 int main(void) {
-
+    iniciar_jogo();
     exibir_menu();
-
     return 0;
 }
